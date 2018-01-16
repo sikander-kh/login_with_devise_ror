52e73d4050f28409ba15dbfdbc00da54e7f81db0
sikanderbhutto <sikander@gkmit.co>
2018-01-16 16:44:51 +0530

    log out option added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def delete_content
+  respond_to do |format|               
+    format.js
+    render "users/sign_in"
+    end        
+  end
 end
